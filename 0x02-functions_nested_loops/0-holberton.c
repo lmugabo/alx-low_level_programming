18d43751e7f1420f2944a7adb6bf7c45e2d51bda
lmugabo <mugaboli@yahoo.com>
2021-06-22 07:19:57 -0700

    add new files

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main-printing using putchar
+ *
+ *Return 0
+ */
+int main(void)
+{
+char c[9]={'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+unsigned int a;
+for (a=0 ;a<= sizeof c;a++)
+_putchar(holberton[c]);
+
+_putchar('\n');
+return (0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# alx-low_level_programming
+Learning low level C programming
