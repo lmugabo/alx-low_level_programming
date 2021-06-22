2952bceeff5fce2589a302f8694bcf8f474453c7
lmugabo <mugaboli@yahoo.com>
2021-06-22 07:29:34 -0700

    add

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -9,7 +9,7 @@ int main(void)
 char holberton[9]={'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
 unsigned int a;
 for (a=0 ;a<= sizeof holberton;a++)
-_putchar(holberton[c]);
+_putchar(holberton[a]);
 
 _putchar('\n');
 return (0);
