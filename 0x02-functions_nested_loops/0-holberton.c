d24f368fc12302ed04a07df7bca2acc0f8eb6fb5
lmugabo <mugaboli@yahoo.com>
2021-06-23 07:46:33 -0700

    add files

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,6 +1,4 @@
 #include "holberton.h"
-
-
 /**
  *main-printing using putchar
  *
@@ -10,9 +8,8 @@ int main(void)
 {
 char holberton[9]={'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
 unsigned int a;
- for (a=0 ;a< sizeof (holberton);a++)
+for (a= 0; a < sizeof (holberton); a++)
 _putchar(holberton[a]);
-
 _putchar('\n');
 return (0);
 }
