3e7b7b97b5012a9be412511a76e9db01a5098efc
lmugabo <mugaboli@yahoo.com>
2021-06-28 12:45:49 -0700

    commit changes

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,6 +10,7 @@ int c, a;
 char l;
 for (c = 0; str[c] != 0; c++)
 {
+}
 if (c % 2 == 0)
 {
 for ((a = c / 2); str[a] != 0; a++)
@@ -24,7 +25,6 @@ for ((a = (c - 1) / 2); str[a] != 0; a++)
 {
 l = str[a];
 _putchar(l);
-}
 }
 }
 _putchar('\n');
