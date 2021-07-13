25c5c54082a3f3f9fdb7b37649293d45e88cef0f
lmugabo <mugaboli@yahoo.com>
2021-07-13 07:00:25 -0700

    corrections

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,6 +10,10 @@ char *create_array(unsigned int size, char c)
 {
 char *my_array;
 unsigned int i = 0;
+if (size == 0)
+{
+return (NULL);
+}
 my_array = malloc(size * sizeof(c));
 while (i < size)
 {
