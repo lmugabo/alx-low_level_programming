93c5407a66df40aca749fbf9b6957b550079f45d
lmugabo <mugaboli@yahoo.com>
2021-07-22 11:06:20 -0700

    add changes

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,8 @@
-#ifndef HOLBERTON_H_FILE
-#define HOLBERTON_H_FILE
+#ifndef FUNCT_PTR
+#define FUNCT_PTR
+#include <stdio.h>
 #include <stdlib.h>
-
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-
 #endif
